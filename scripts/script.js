b5bb873a2accd891c6b43b2dec2412d35cf975ea
KC Lopez <khalelclarklopez@gmail.com>
2023-11-08 10:09:26 -0800

    added logout function

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,12 @@ function sayHello() {
     
 }
 //sayHello();
+
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
