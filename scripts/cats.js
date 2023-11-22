85d54103d4fdc6d8c9a70e9b2be1dfcd1d36d879
KC Lopez <khalelclarklopez@gmail.com>
2023-11-22 08:36:49 -0800

    added placeholder game page + some functionality

--- scripts/cats.js
+++ scripts/cats.js
@@ -1,37 +1,78 @@
 //declaring variables waw
 const selectedCat = "farmerCat";
-const cat = db.collection("users").doc("xYwKXL8oCeTrSLyuwjbXYodDhCI2").collection("game").doc(selectedCat);
+// const selectedCat = db.collection("users").doc("xYwKXL8oCeTrSLyuwjbXYodDhCI2").collection("game").doc("userStats");
+const game = db.collection("users").doc("xYwKXL8oCeTrSLyuwjbXYodDhCI2").collection("game").doc("userInfo");
 const FieldValue = firebase.firestore.FieldValue;
 
 //links html to the js, waw
 const upgradeBtn = document.getElementById("upgrade-button");
 const resourcesDisplay = document.getElementById("resources-goes-here");
 const powerDisplay = document.getElementById("power-goes-here");
+const nameDisplay = document.getElementById("name-goes-here");
+const healthDisplay = document.getElementById("health-goes-here");
+
+var upgrading = false;
 
 //little console log to see if the code is even up and running
 console.log("code is running")
 
+//updates power stat of a cat
+function updateStats(){
+
+    let cat = db.collection("users").doc("xYwKXL8oCeTrSLyuwjbXYodDhCI2").collection("game").doc(selectedCat);
+
+    cat.get().then( DOC => {
+
+        let power = DOC.data().power;
+        let powerIncrement = DOC.data().powerIncrement;
+
+        let health = DOC.data().health;
+        let healthIncrement = DOC.data().healthIncrement;
+
+        let name = DOC.data().name;
+
+
+        if (upgrading == true){
+            cat.update({
+
+                power: power + powerIncrement,
+                health: health + healthIncrement
+
+            });
+
+            upgrading = false;
+
+        }
+
+        nameDisplay.innerHTML = name;
+        powerDisplay.innerHTML = "Power: " + power;
+        healthDisplay.innerHTML = "Health: " + health;
+
+    });
+}
+
+updateStats();
+
 //upgrade function (only power for now)
-function upgradePwr(){
+function upgradeStats(){
 
     //get user doc then...
-    cat.get().then( DOC => {
+    game.get().then( DOC => {
 
         //get user data for "resources"
         let resources = DOC.data().resources;
-        let power = DOC.data().power;
 
         if (resources > 0){
 
             //reduce resources by 1, increase power by 1
-            cat.update({
-                resources: resources - 1,
-                power: power + 1
+            game.update({
+                resources: resources - 1
             });
 
             //update display
             resourcesDisplay.innerHTML = "Resources left: " + resources;
-            powerDisplay.innerHTML = "Power: " + power;
+            updateStats();
+
         }
         else {
             console.log("No resources left :(");
@@ -41,4 +82,4 @@ function upgradePwr(){
 }
 
 //on click, upgrade power stat
-upgradeBtn.addEventListener("click", upgradePwr); 
+upgradeBtn.addEventListener("click", upgradeStats); 
