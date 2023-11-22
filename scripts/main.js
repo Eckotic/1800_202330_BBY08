b61b907c43ea50307ad6bab124f7d28b350bea33
Ethan Nguyen <nethang4@gmail.com>
2023-11-21 17:47:58 -0800

    new main page layout and firestore sleep history

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,65 @@
-function getNameFromAuth() {
+function insertNameFromFirestore() {
+    // Check if the user is logged in:
     firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
         if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            var myArray = user.displayName.split(" ");
-            var userName = myArray[0];
-
-            //method #1:  insert with JS
-            document.getElementById("name-goes-here").innerText = userName;
-
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
+            currentUser.get().then(userDoc => {
+                // Get the user name
+                var userName = userDoc.data().name;
+                console.log(userName);
+                //$("#name-goes-here").text(userName); // jQuery
+                var myArray = userName.split(" ");
+                document.getElementById("name-goes-here").innerText = myArray[0];
+            })
         } else {
-            // No user is signed in.
+            console.log("No user is logged in."); // Log a message when no user is logged in
         }
-    });
+    })
 }
-getNameFromAuth(); //run the function
+
+insertNameFromFirestore();
 
 // Get the elements by their ID
 var popupLink = document.getElementById("sleep-button");
 var popupWindow = document.getElementById("sleep-popup");
 var closeSleepButton = document.getElementById("cancel-sleep-button");
 var confirmButton = document.getElementById("confirm-sleep-button");
-var ignoreButton = document.getElementById("ignore-sleep-popup");
+var ignoreButton1 = document.getElementById("ignore-sleep-popup");
 var sessionWindow = document.getElementById("session-popup");
 var closeSessionButton = document.getElementById("close-window");
 var sleepLogButton = document.getElementById("sleep-log");
 var sleepGameButton = document.getElementById("sleep-game");
 
-var ignore = false;
+var endSleepWindow = document.getElementById("end-sleep-popup");
+var endSession = document.getElementById("end-session");
+var confirmSessionButton = document.getElementById("confirm-end-sleep-button");
+var cancelSessionButton = document.getElementById("cancel-end-sleep-button");
+var ignoreButton2 = document.getElementById("ignore-end-sleep-popup");
+
+var ignore1 = false;
+var ignore2 = false;
 
 sleepLogButton.addEventListener("click", function () {
     sessionWindow.style.display = "none";
-    finishSession();
 });
 
 sleepGameButton.addEventListener("click", function () {
     sessionWindow.style.display = "none";
-    finishSession();
 });
 
 closeSessionButton.addEventListener("click", function () {
     sessionWindow.style.display = "none";
-    finishSession();
 });
+
+
 
 // Show the pop-up window when the link is clicked
 popupLink.addEventListener("click", function (event) {
-    if (ignore == false) {
+    if (ignore1 == false) {
         event.preventDefault();
         popupWindow.style.display = "block";
-    }   else {
+    } else {
         sleepMode();
     }
 });
@@ -62,11 +70,41 @@ closeSleepButton.addEventListener("click", function () {
 
 confirmButton.addEventListener("click", function () {
     popupWindow.style.display = "none";
-    if (ignoreButton.checked) {
-        ignore = true;
+    if (ignoreButton1.checked) {
+        ignore1 = true;
     }
     sleepMode();
 });
+
+endSession.addEventListener("click", function (event) {
+    if (ignore2 == false) {
+        event.preventDefault();
+        endSleepWindow.style.display = "block";
+    } else {
+        off();
+    }
+
+    const sleepTime = displayTime.split(":");
+    const sleepMinutes = sleepTime[1];
+
+    if (sleepMinutes < 20) {
+        let sessionWarning = document.getElementById("session-warning");
+        sessionWarning.style.display = "block";
+        sessionWarning.innerHTML = "Sessions less than 30 minutes will not be logged!"
+    }
+});
+
+confirmSessionButton.addEventListener("click", function () {
+    endSleepWindow.style.display = "none";
+    if (ignoreButton2.checked) {
+        ignore2 = true;
+    }
+    off();
+})
+
+cancelSessionButton.addEventListener("click", function () {
+    endSleepWindow.style.display = "none";
+})
 
 function sleepMode() {
     document.getElementById("overlay").style.display = "block";
@@ -75,25 +113,45 @@ function sleepMode() {
 
 function off() {
     document.getElementById("overlay").style.display = "none";
+    document.getElementById("sleep-button").innerHTML = "Sleep";
     stopStopwatch();
-    sessionWindow.style.display = "block";
+    finishSession();
+    sleepSession();
 }
 
 var startTime; // to keep track of the start time
 var stopwatchInterval; // to keep track of the interval
 var elapsedPausedTime = 0; // to keep track of the elapsed time while stopped
+var displayTime // the time of sleep
+
+let startDate;
+let endDate;
 
 function startStopwatch() {
-  if (!stopwatchInterval) {
-    startTime = new Date().getTime() - elapsedPausedTime; // get the starting time by subtracting the elapsed paused time from the current time
-    stopwatchInterval = setInterval(updateStopwatch, 1000); // update every second
-  }
+    if (!stopwatchInterval) {
+        startTime = new Date().getTime() - elapsedPausedTime; // get the starting time by subtracting the elapsed paused time from the current time
+        stopwatchInterval = setInterval(updateStopwatch, 1000); // update every second
+    }
+
+    const date = new Date();
+
+    let currentDay = String(date.getDate()).padStart(2, '0');
+
+    let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+
+    let currentYear = date.getFullYear();
+
+    // we will display the date as MM.DD.YYYY 
+
+    startDate = `${currentMonth}.${currentDay}.${currentYear}`; 
+
 }
 
 function stopStopwatch() {
-  clearInterval(stopwatchInterval); // stop the interval
-  elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
-  stopwatchInterval = null; // reset the interval variable
+    clearInterval(stopwatchInterval); // stop the interval
+    elapsedPausedTime = new Date().getTime() - startTime; // calculate elapsed paused time
+    stopwatchInterval = null; // reset the interval variable
+    elapsedPausedTime = 0;
 }
 
 function updateStopwatch() {
@@ -102,15 +160,55 @@ function updateStopwatch() {
     var seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
     var minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
     var hours = Math.floor(elapsedTime / 1000 / 60 / 60); // calculate hours
-    var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); // format display time
+    displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); // format display time
     document.getElementById("sleep-button").innerText = displayTime; // update the display
 }
-  
+
 function pad(number) {
     // add a leading zero if the number is less than 10
     return (number < 10 ? "0" : "") + number;
 }
 
 function finishSession() {
-    return;
+
+    // Date object
+    const date = new Date();
+
+    let currentDay = String(date.getDate()).padStart(2, '0');
+
+    let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+
+    let currentYear = date.getFullYear();
+
+    // we will display the date as MM.DD.YYYY 
+
+    endDate = `${currentMonth}.${currentDay}.${currentYear}`;
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
+            currentUser = db.collection("users").doc(user.uid).collection("sleephistory").doc(endDate).set({
+                sleeplength: displayTime
+            }); // Go to the Firestore document of the user
+
+        }
+    })
+
+    console.log("sleep logged!")
+
+}
+
+function sleepSession() {
+
+    sessionWindow.style.display = "block";
+
+    const sleepTime = displayTime.split(":");
+
+    const sleepMinutes = sleepTime[1];
+    const sleepHours = sleepTime[0];
+
+    document.getElementById("start-date").innerHTML = "Sleep Date Start: " + startDate;
+    document.getElementById("end-date").innerHTML = "Sleep Date End: " + endDate;
+    document.getElementById("sleep-length").innerHTML = "Length of Sleep: " + sleepHours + " Hours, " + sleepMinutes + " Minutes ";
+
 }
