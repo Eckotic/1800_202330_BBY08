34c07994ece851478d284720aec61a4c813fed25
Ethan Nguyen <nethang4@gmail.com>
2023-12-06 07:13:12 -0800

    Edit tracker

--- scripts/tracker.js
+++ scripts/tracker.js
@@ -31,11 +31,43 @@ confirmButton.addEventListener("click", function () {
 });
 
 
+const day1 = document.getElementById("day-1");
+const length1 = document.getElementById("length-1");
+const day2 = document.getElementById("day-2");
+const length2 = document.getElementById("length-2");
+const day3 = document.getElementById("day-3");
+const length3 = document.getElementById("length-3");
+const day4 = document.getElementById("day-4");
+const length4 = document.getElementById("length-4");
+
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
-        currentUser = db.collection("users").doc(user.uid).collection("sleephistory").get();
-        console.log(currentUser);
-        
+        currentUser = db.collection("users").doc(user.uid).collection("sleephistory");
 
-    }
-})
+        currentUser.doc("11.28.2023").get().then(userDoc => {
+            day1.innerHTML = "Date: 11.28.2023";
+            length1.innerHTML = "Sleep Length: " + userDoc.data().sleeplength;
+
+
+        })
+
+        currentUser.doc("11.29.2023").get().then(userDoc => {
+            day2.innerHTML = "Date: 11.29.2023";
+            length2.innerHTML = "Sleep Length: " + userDoc.data().sleeplength;
+        })
+
+        currentUser.doc("12.05.2023").get().then(userDoc => {
+
+            day3.innerHTML = "Date: 12.05.2023";
+            length3.innerHTML = "Sleep Length: " + userDoc.data().sleeplength;
+
+        })
+
+        currentUser.doc("12.06.2023").get().then(userDoc => {
+            day4.innerHTML = "Date: 12.06.2023";
+            length4.innerHTML = "Sleep Length: " + userDoc.data().sleeplength;
+
+        })
+
+    };
+});
